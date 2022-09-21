d54741496ad1253cd41302c6161ec94eddc48f8e
Harshit Jain <109205787+harshitjain25@users.noreply.github.com>
2022-09-21 20:25:50 +0530

    Add files via upload

---
+++ linked_list/insertionAtEnd.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+int main()
+{
+    int a;
+    struct node *start = NULL, *p = NULL, *p1 = NULL, *t = NULL;
+    char k;
+    start = (struct node *)malloc(sizeof(struct node));
+    printf("Enter Data: ");
+    scanf("%d", &(start->data));
+    start->next = NULL;
+    p = start;
+    do
+    {
+        p1 = (struct node *)malloc(sizeof(struct node));
+        printf("Enter Data: ");
+        scanf("%d", &(p1->data));
+        p1->next = NULL;
+        p->next = p1;
+        p = p1;
+        getchar();
+        printf("Want to add more? ");
+        scanf("%c", &k);
+
+    } while (k == 'y');
+    printf("Enter the number to insert: ");
+    scanf("%d", &a);
+    t = start;
+    while (t->next != NULL)
+    {
+        t = t->next;
+    }
+    p1 = (struct node *)malloc(sizeof(struct node));
+    p1->next = NULL;
+    t->next = p1;
+    p1->data = a;
+    t = start;
+
+    while (t != NULL)
+    {
+
+        printf("%d ", t->data);
+        t = t->next;
+    }
+
+    return 0;
+}
